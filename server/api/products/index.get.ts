97e472d74de6c810e8c2bf42425f64e1b77a063f
jb222 <jb222@hdm-stuttgart.de>
2022-12-30 03:14:17 +0100

    fixing product type

--- server/api/products/index.get.ts
+++ server/api/products/index.get.ts
@@ -3,6 +3,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
-  const products = prisma.product.findMany();
+  const products = await prisma.product.findMany();
   return products;
 });
--- types/Product.ts
+++ types/Product.ts
@@ -1,5 +1,5 @@
 export interface Product {
-  id: string;
+  id: number;
   name: string;
   color: string;
   price: string;
